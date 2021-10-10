b93980493b8d3c8836cf569325ca2c3404ec0317
Senimtra <senimtra@hotmail.com>
2021-10-10 19:10:50 +0200

    solve iteration 1: create the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="hero-section">
+          <img src="./images/ironhack-logo.svg" alt="ironhack-logo" />
+          <div id="message">
+            <h1>Say hello to ReactJS</h1>
+            <p>
+              You will learn a Frontend framework from scratch, to become a
+              Ninja Developer.
+            </p>
+          </div>
+          <button>Awesome!</button>
+        </div>
+        <div>
+          <img src="./images/menu-top.svg" alt="menu-top" />
+        </div>
+        <img
+          id="logo-1"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
+        <img
+          id="logo-2"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
+        <img
+          id="logo-3"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
+        <img
+          id="logo-4"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
+        <img
+          id="logo-5"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
+        <img
+          id="logo-6"
+          class="react-logo"
+          src="./images/react-logo.svg"
+          alt="react-logo"
+        />
       </div>
     );
   }
